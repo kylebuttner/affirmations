36d8c5adb6cd937f7dfec2e3e22e125836cb75d0
Kyle Buttner <kylebuttner@gmail.com>
2016-06-04 15:24:02 +0000

    image displays on homepage

--- tests/e2e-tests/homepage.tests.js
+++ tests/e2e-tests/homepage.tests.js
@@ -6,4 +6,9 @@ describe('Homepage', function() {
   it('should have a title', function() {
     expect(browser.getTitle()).toEqual('The Positive Birth Company');
   });
+
+  it('should display correct daily image', function() {
+    var dailyPic = element(by.css("img[src='http://www.planwallpaper.com/static/images/apple-iphone4s-wallpaper-26.jpg']"));
+    expect((dailyPic).isPresent()).toBe(true);
+  })
 });
